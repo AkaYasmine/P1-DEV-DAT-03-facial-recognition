cd044c64afecd408c9ae693790240fb1467b6220
mouna005 <167528737+mouna005@users.noreply.github.com>
2024-09-11 11:00:42 +0000

    [configuration] définition de la structure du Front

--- my-projet-app/src/pages/Gestion_employés.js
+++ my-projet-app/src/pages/Gestion_employés.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Body from '../components/Public/Body';
+import NavBar from '../components/NavBar';
 
 const Services = () => {
     return (
         <div>
-        Interface Gestion employées
+        <NavBar/>
         <Body/>
             
         </div>
--- my-projet-app/src/pages/Pointage_employés.js
+++ my-projet-app/src/pages/Pointage_employés.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Body from '../components/Public/Body';
+import NavBar from '../components/NavBar';
 
 const Pointage_employées = () => {
     return (
         <div>
          Interface poinatge employés
+         <NavBar/>
         <Body/>  
         </div>
     );
