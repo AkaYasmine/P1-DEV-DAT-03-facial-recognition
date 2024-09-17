211411d3ed8f8e5e3ab343e8cea5b94d447794bf
VETCHO06 <herve.ouattara@epitech.eu>
2024-09-17 09:25:00 +0000

    Dashboard Pogban97

--- my-projet-app/src/components/Public/Homepage.js
+++ my-projet-app/src/components/Public/Homepage.js
@@ -14,7 +14,7 @@ const HomePage = () => {
       <div className="relative p-10 rounded-lg shadow-2xl max-w-lg w-full">
         <h1 className="text-4xl font-extrabold text-gray-800 mb-4 text-center">Bienvenue à WE.CODE</h1>
         <p className="text-gray-700 text-center mb-8 text-xl">
-          Connecte-vous et gérez les employés de l'entreprise.
+          Connectez-vous et gérez les employés de l'entreprise.
         </p>
         <div className="flex justify-center">
           <button onClick={goToConnexion} className="bg-yellow-500 text-white px-8 py-3 rounded-lg shadow-lg transform transition-transform duration-300 hover:scale-105 hover:bg-yellow-600">
@@ -25,5 +25,4 @@ const HomePage = () => {
     </div>
   );
 };
-
 export default HomePage;
--- my-projet-app/src/pages/Inscription.js
+++ my-projet-app/src/pages/Inscription.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import NavBar from '../components/NavBar';
 import Inscription from '../components/Public/Inscription';
 
 const Services = () => {
     return (
         <div>
-        <NavBar/>
         <Inscription />
         </div>
     );
